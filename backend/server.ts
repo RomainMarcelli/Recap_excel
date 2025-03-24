92e1ee9ef0c0f8ac07528846c92365a2c168bd06
Romain MARCELLI <rmarcelli@systelecoms.fr>
2025-03-24 16:41:35 +0100

    ajout connexion admin

--- backend/server.ts
+++ backend/server.ts
@@ -5,21 +5,21 @@ import dotenv from "dotenv";
 import projectRoutes from "./routes/projectsRoute";
 import collaboratorRoutes from "./routes/collaboratorRoutes";
 import tjmRoutes from "./routes/tjmRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "https://deploiement-recap-excel-i1to.vercel.app",
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/projects", projectRoutes);
 app.use("/collaborators", collaboratorRoutes);
 app.use("/api/tjm", tjmRoutes);
+app.use("/auth", userRoutes); // ➜ /auth/login et /auth/register
+
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
